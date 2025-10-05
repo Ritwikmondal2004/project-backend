2a348b2a95634a57f9ddb780e39a6819ababc68b
Ritwik Mondal <ritwik8575@gmail.com>
2025-10-05 18:37:14 +0530

    backend

--- src/middlewares/multer.middlewar.js
+++ src/middlewares/multer.middlewar.js
@@ -1,4 +1,5 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -10,6 +11,8 @@ const storage = multer.diskStorage({
     }
   })
   
-export const upload = multer({ 
+const upload = multer({ 
     storage, 
-})
+});
+
+export default upload;
