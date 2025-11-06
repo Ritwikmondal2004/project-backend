6585009bb1ecae800266b7bb61590bb35590ec2f
Ritwikmondal2004 <ritwik8575@gmail.com>
2025-11-06 17:16:06 +0530

    Refactor tweet model: format code for consistency and readability

--- project-backend-main/src/models/tweet.model.js
+++ project-backend-main/src/models/tweet.model.js
@@ -2,14 +2,14 @@ import mongoose, { Schema } from "mongoose";
 
 const tweetSchema = new Schema(
   {
-    content:{
-        type: String,
-        required:true
+    content: {
+      type: String,
+      required: true,
     },
-    owner:{
-        type:Schema.Types.ObjectId,
-        ref:"User"
-    }
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
   {
     timestamps: true,
